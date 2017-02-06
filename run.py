096ff3a5ae8acf7a6db7209429ad787e38cd96ad
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2017-02-05 23:53:57 -0500

    run.py:

--- run.py
+++ run.py
@@ -5,18 +5,21 @@
 
 import subprocess
 import numpy as np
-
-algorithm = 'lac'
-#algorithm = 'kmeans'
+import re
+#algorithm = 'lac'
+algorithm = 'kmeans'
 #algorithm = 'kmedoids'
 
 command = 'python3.4 %s.py' %algorithm
 
 n = 10
-output = np.zeros(n)
+output = np.zeros((n,2))
+print('Accuracy      Silhouette')
 for i in range(n):
    temp_output = subprocess.check_output(command, shell=True)
-   temp_output = float(temp_output)
-   output[i] = temp_output
-   print("accuracy:", temp_output)
-print(np.mean(output), '+/-', np.std(output))
+   temp_output = str(temp_output)
+   regex = re.findall(r'[-+]?\d*\.\d+|\d+', temp_output)
+   output[i,:] = [regex[0], regex[1]]
+   print(output[i,:])
+print('Mean:', np.mean(output, axis=0))
+print('Std :', np.std(output, axis=0))
