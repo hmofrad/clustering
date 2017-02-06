8a841adcc9fd94e473bed7ba3de0eebfc947d801
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2017-02-05 20:52:03 -0500

    run.py:

--- run.py
+++ run.py
@@ -6,18 +6,17 @@
 import subprocess
 import numpy as np
 
-algorithm = 'kmeans'
+algorithm = 'lac'
+#algorithm = 'kmeans'
+#algorithm = 'kmedoids'
 
 command = 'python3.4 %s.py' %algorithm
-output = []
 
 n = 10
 output = np.zeros(n)
-
 for i in range(n):
    temp_output = subprocess.check_output(command, shell=True)
    temp_output = float(temp_output)
    output[i] = temp_output
    print("accuracy:", temp_output)
 print(np.mean(output), '+/-', np.std(output))
-
