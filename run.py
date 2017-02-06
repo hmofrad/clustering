e9ee040251e8f15857418e8c194a1d8e3a89f771
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2017-02-05 20:47:58 -0500

    run.py:

---
+++ run.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.4
+# Experiments 
+# (c) Mohammad H. Mofrad
+# (e) hasanzadeh@cs.pitt.edu
+
+import subprocess
+import numpy as np
+
+algorithm = 'kmeans'
+
+command = 'python3.4 %s.py' %algorithm
+output = []
+
+n = 10
+output = np.zeros(n)
+
+for i in range(n):
+   temp_output = subprocess.check_output(command, shell=True)
+   temp_output = float(temp_output)
+   output[i] = temp_output
+   print("accuracy:", temp_output)
+print(np.mean(output), '+/-', np.std(output))
+
