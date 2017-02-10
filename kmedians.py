a5c4b6f2edecbf9b0254d54a4e82992b41a077b1
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2017-02-09 19:12:22 -0500

    kmedians.py: Added kmedian clustering

---
+++ kmedians.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3.4
+# kmedians implementation
+# (c) Mohammad H. Mofrad, 2017 
+# (e) hasanzadeh@cs.pitt.edu
+
+import numpy as np
+from utils import *
+
+FILE = 'iris.data.txt'
+#FILE = 'glass.data.txt'
+
+# Read input file 
+# using the utils.py
+[x, y] = read(FILE)
+
+# Initliaze parameters
+[n, d] = np.shape(x)   # [#samples, #dimensions]
+k = len(np.unique(y))  #  #clusters
+
+mi = np.min(x, axis=0) # Minimum
+ma = np.max(x, axis=0) # Maximum
+di = ma - mi           # Difference
+stop = 0               # Stopping criterion 
+
+c = np.zeros(n)        # Cluster membership 
+med = np.random.rand(k, d) * np.ones((k, d)) # Clusters median
+med = med * di
+med = med + mi
+med_t = np.copy(med) # Copy of clusters median
+
+imax = 100
+for i in range(imax):
+   med_t = np.copy(med)
+  
+   # Calculate minimum Euclidean distance and
+   # update clusters membership
+   for j in range(n):
+      dist = np.sqrt(np.sum(np.power(x[j,:] - med, 2), axis=1))
+      idx = np.argmin(dist)
+      val = np.min(dist)
+      c[j] = idx
+   
+   # Calculate cluster membership and
+   # update clusters median
+   for j in range(k):
+      a = np.arange(n)
+      idx = a[c == j] # Current cluster
+      l = len(idx)    # #cluster elements
+      if l:
+         med[j,:] = np.median(x[idx,:], axis=0)
+      else:
+         med[j,:] = med[j,:] + (np.random.rand(d) * di)
+
+   # Check against stopping criterion
+   st = np.sum(np.sum(np.power(med - med_t,2), axis=0))
+   if(stop <= 0) or (i >= imax):
+      break
+
+# Calculate accuracy and
+# Silhouette Coefficient
+# using the utils.py
+acc = accuracy(c, y, k)
+sil = silhouette(x, c, med)
+print(acc, sil)
