7186a02cc46f0c7acbc70faff6c061b8d774c95e
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2017-05-16 21:53:14 -0400

    kmedians.py:

--- kmedians.py
+++ kmedians.py
@@ -9,7 +9,7 @@ from utils import *
 # Read and store the input data
 # using the utils.py
 PERFIX = 'dataset/'
-FILE = PERFIX + 'balance-scale.data.txt'
+#FILE = PERFIX + 'balance-scale.data.txt'
 #FILE = PERFIX + 'breast-cancer-wisconsin.data.txt'
 #FILE = PERFIX + 'sonar.all-data.txt'
 #FILE = PERFIX + 'cmc.data.txt'
@@ -19,6 +19,8 @@ FILE = PERFIX + 'balance-scale.data.txt'
 #FILE = PERFIX + 'iris.data.txt'
 #FILE = PERFIX + 'pima-indians-diabetes.data.txt'
 #FILE = PERFIX + 'wine.data.txt'
+#FILE = PERFIX + 'drift.data.txt'
+FILE = PERFIX + 'har.data.txt'
 
 [x, y] = read(FILE)
 
